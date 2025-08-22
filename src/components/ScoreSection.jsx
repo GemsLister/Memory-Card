7cf9eb8a1d17d1d3115a5d24fecb5d2af8075bd1
GemsLister <jameslester.l.lopez@gmail.com>
2025-08-22 10:45:02 +0800

    feat:add new props for scoring purposes

--- src/components/ScoreSection.jsx
+++ src/components/ScoreSection.jsx
@@ -1,33 +1,37 @@
-import { useState, useEffect } from "react";
-export const ScoreSection = ({ pokemonClicked }) => {
-  const [score, setScore] = useState();
+import { useEffect, useState } from "react";
+export const ScoreSection = ({
+  setPokemonClicked,
+  pokemonClicked,
+  score,
+  setScore
+}) => {
+  const [highScore, setHighScore] = useState(0);
   useEffect(() => {
     let handleScoreChange = () => {
-      setScore([0]);
       pokemonClicked.filter((item, index) => {
         if (pokemonClicked.indexOf(item) !== index) {
           console.log("Game Over");
-          pokemonClicked.length = 0;
-        } else {
-          console.log(`Selected Pokémon ID: ${item} ${index}`);
+          setScore(0);
+          setHighScore((prevHighScore) => Math.max(prevHighScore, score));
+          setPokemonClicked([]);
+        } else if (pokemonClicked.length > 0) {
+          setScore(score + 1);
+          console.log(`Selected Pokémon ID: ${item}, Index: ${index}`);
         }
         console.log(
-          `Current Pokémon Array: ${pokemonClicked}, ${pokemonClicked.length}`
+          `Current Pokémon Array: ${pokemonClicked}, Length: ${
+            pokemonClicked.length
+          }, Score: ${score + 1}`
         );
       });
     };
     handleScoreChange();
-    setScore([pokemonClicked.length]);
-    if (pokemonClicked.length === 12) {
-      console.log("You win!");
-      pokemonClicked.length = 0;
-    }
   }, [pokemonClicked]);
 
   return (
     <section className="score-section font-bitcount text-[1.5rem] text-white">
       <h1>Score: {score}</h1>
-      <h1>High Score: 0</h1>
+      <h1>High Score: {highScore}</h1>
     </section>
   );
 };
