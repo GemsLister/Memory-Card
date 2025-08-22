bb2a71fb800e7d50ceb4fdf1e8cde7032f6199b0
GemsLister <jameslester.l.lopez@gmail.com>
2025-08-22 10:43:30 +0800

    fix: change window.location.reload into fisher-yates algorithm

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 
-export const CardsSection = ({ pokeDetails, setPokemonClicked }) => {
+export const CardsSection = ({
+  pokeDetails,
+  setPokemonClicked,
+  setPokeDetails,
+}) => {
   const clickHandler = (pokeID) => {
     console.log(`Clicked Pokémon ID: ${pokeID}`);
     // Checks if the pokemon ID is null for inserting an ID in the array
@@ -9,8 +13,26 @@ export const CardsSection = ({ pokeDetails, setPokemonClicked }) => {
     );
   };
 
-  const restartPage = () => {
-    window.location.reload();
+  const shuffleArray = (pokeDetails) => {
+    // Copy the pokeDetails array
+    let shuffle = [...pokeDetails];
+    // Minus 1 to avoid overlap
+    let i = shuffle.length - 1;
+    let j;
+    while (i > 0) {
+      // Generating random number from 0 - 12
+      j = Math.floor(Math.random() * (i + 1));
+      // Decrement to make its loop 12 time
+      i--;
+      // Swapping the current cards and random cards
+      [shuffle[i], shuffle[j]] = [shuffle[j], shuffle[i]]; 
+    }
+    return shuffle;
+  };
+
+  const reloadCards = () => {
+    // Reshuffle another new array of cards
+    setPokeDetails(prev => shuffleArray(prev)); 
   };
 
   useEffect(() => {
@@ -28,7 +50,7 @@ export const CardsSection = ({ pokeDetails, setPokemonClicked }) => {
               className="flex flex-col items-center justify-center bg-poke-bluegray rounded-[8px] sm:w-[100%] h-[100%] cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out hover:bg-poke-bluegray/80 active:bg-poke-bluegray/60 shadow-header text-poke-text"
               onClick={() => {
                 clickHandler(_sprites.id);
-                restartPage();
+                reloadCards();
               }}
             >
               <img
